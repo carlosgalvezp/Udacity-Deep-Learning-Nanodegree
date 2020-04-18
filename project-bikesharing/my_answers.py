786bff1c9b1d58c1dfb9c84ca646fd60841e74ef
Carlos Gálvez <carlosgalvezp@gmail.com>
2020-04-18 21:22:59 +0200

    Finish P1

--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -132,7 +132,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+iterations = 6000
+learning_rate = 0.3
+hidden_nodes = 16
 output_nodes = 1
